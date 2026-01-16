db988a7600f712e7bb4967e6bd4de6a73fe82ab6
Markus Thiede <mailtothemaru@gmail.com>
2026-01-16 12:10:21 +0100

    added output of the body to main

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -36,6 +36,8 @@ func main() {
 		for key, value := range request.Headers {
 			fmt.Printf("- %s: %s\n", key, value)
 		}
+		fmt.Println("Body:")
+		fmt.Println(string(request.Body))
 
 		fmt.Println("Connection is closed again")
 	}
